9abe793239b9ac3bdd41c70864f09eaf21f20cd6
mariussgal <marius.gal@esme.fr>
2025-06-04 22:26:22 +0200

    Added  ThreeButtonComponent to change between diff modes

---
+++ src/app/components/ThreeButtonComponent.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Play, Pause, Square } from 'lucide-react';
+
+interface ThreeButtonComponentProps {
+  onButton1Click?: () => void;
+  onButton2Click?: () => void;
+  onButton3Click?: () => void;
+  selectedMode?: "DAE" | "ARRET" | "Moniteur";
+}
+const ThreeButtonComponent: React.FC<ThreeButtonComponentProps> = ({
+  onButton1Click,
+  onButton2Click,
+  onButton3Click,
+  selectedMode
+}) => {
+  return (
+    <div className="flex items-center justify-center">
+      <button
+        onClick={onButton1Click}
+        className={`w-16 h-10 ${selectedMode === "DAE" ? "bg-yellow-400" : "bg-green-600"} active:bg-yellow-400 p-4 transition-all touch-manipulation flex items-center justify-center`}
+      >
+        <span className="text-black text-xs font-bold">DAE</span>
+      </button>
+      
+      <button
+        onClick={onButton2Click}
+        className={`w-11 h-10 ${selectedMode === "ARRET" ? "bg-yellow-400" : "bg-white"} active:bg-yellow-400 p-4  transition-all touch-manipulation flex items-center justify-center`}
+      >
+        <span className="text-black text-xs font-bold">ARRET</span>
+      </button>
+      
+      <button
+        onClick={onButton3Click}
+        className={`w-16 h-10 ${selectedMode === "Moniteur" ? "bg-yellow-400" : "bg-green-600"} active:bg-yellow-400 p-4  transition-all touch-manipulation flex items-center justify-center`}
+      >
+        <span className="text-black text-xs font-bold">Moniteur</span>
+      </button>
+    </div>
+  );
+};
+
+export default ThreeButtonComponent; 
